198d004286c0281fe9f5ebb12ddcf795c78f4f26
esmangle <esmendaangelo@gmail.com>
2025-07-08 17:18:38 +0800

    set up some db functions

--- app/db.php
+++ app/db.php
@@ -3,12 +3,67 @@
 declare(strict_types = 1);
 
 require_once '../env.php';
+require_once '../app/user.php';
 
 class DB {
+	public static array $testdb = [
+		[
+			'name' => 'test',
+			'pass' => 'password',
+		],
+		[
+			'name' => 'test2',
+			'pass' => 'password2',
+		],
+	];
+
+	private static array $cachedUsers = [];
+
+	private final function __construct() {}
+
 	public static function isUserExisting(string $username): bool {
+		foreach (self::$testdb as $entry) {
+			if ($entry['name'] === $username) {
+				return true;
+			}
+		}
+
 		return false;
 	}
 
-	public static function newUser(string $username, string $password): void {
+	public static function getUserFromId(int $id): ?User {
+		$user = self::$cachedUsers[$id] ?? null;
+
+		if ($user) {
+			return $user;
+		}
+
+		$data = self::$testdb[$id] ?? null;
+
+		if ($data === null) {
+			return null;
+		}
+
+		$user = new User(
+			id: $id,
+			name: $data['name'],
+			creation_date: new \DateTimeImmutable()
+		);
+
+		self::$cachedUsers[$id] = $user;
+
+		return $user;
+	}
+
+	public static function newUser(string $username, string $password): ?User {
+		return null;
+	}
+
+	public static function loginUser(string $username, string $password): ?User {
+		if (!self::isUserExisting($username)) {
+			return null;
+		}
+
+		return null;
 	}
 }
