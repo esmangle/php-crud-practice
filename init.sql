f2cd0454dc271c244e1136c77d7f8b63a9b32d10
esmangle <esmendaangelo@gmail.com>
2025-07-08 12:53:41 +0800

    update db schema

--- init.sql
+++ init.sql
@@ -4,30 +4,32 @@ USE phpPracticeDB;
 
 CREATE TABLE users (
 	id int UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-	username varchar(32) UNIQUE NOT NULL,
-	bio varchar(500) NOT NULL DEFAULT '',
-	password varchar(60) NOT NULL,
+	username varchar(20) CHARACTER SET ascii COLLATE ascii_general_ci UNIQUE NOT NULL,
+	bio varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+	password char(60) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
 	creation_date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE posts (
-	id int UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	id bigint UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	author_id int UNSIGNED NOT NULL,
-	parent_id int UNSIGNED DEFAULT NULL,
-	content varchar(500) NOT NULL,
-	likes int DEFAULT 0,
-	replies int DEFAULT 0,
+	parent_id bigint UNSIGNED DEFAULT NULL,
+	content varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+	likes int UNSIGNED DEFAULT 0,
+	replies int UNSIGNED DEFAULT 0,
 	deleted boolean DEFAULT 0,
 	creation_date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE,
-	FOREIGN KEY (parent_id) REFERENCES posts(id) ON DELETE SET NULL
+	FOREIGN KEY (parent_id) REFERENCES posts(id) ON DELETE SET NULL,
+	INDEX (creation_date)
 );
 
 CREATE TABLE likes (
 	user_id int UNSIGNED NOT NULL,
-	post_id int UNSIGNED NOT NULL,
+	post_id bigint UNSIGNED NOT NULL,
 	creation_date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (user_id, post_id),
 	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-	FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE
+	FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+	INDEX (creation_date)
 );
